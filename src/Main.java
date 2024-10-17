0594c6928f2ee3c7836f599f7812617bdbd4f89d
Your Name Olya <gorgulaolya@gmail.com>
2024-10-17 16:26:46 +0300

    Added class Task1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        List<String> names = Arrays.asList("John", "Bill");
+        Stream<String> nameStream = names.stream();
+        nameStream.map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("A");
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
+
+
+        Stream<Integer> even = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                        .stream()
+                        .filter(number -> number%2 == 0);
+
+
+        Stream<String> names1 =
+                Arrays.asList("John", "Bill", "Max", "Alex")
+                        .stream()
+                        .filter(name -> name.length() > 3)
+                        .map(name -> "Mr/Ms " + name)
+                        .sorted()
+                        .limit(2);
+
+        Stream<Integer> intStream = Stream.of(1, 2, 5);
+
+        Stream<String> intStream1 = Stream.of("1", "2", "5");
+
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("A");
+                })
+                .forEach(s -> System.out.println("forEach: " + s));
     }
 }
---
+++ src/Task1.java
@@ -0,0 +1,62 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Task1 {
+
+    public static String notAnEvenIndex(List<String> listNames){
+        int i=0;
+        StringBuilder stringName = new StringBuilder();
+        for (String name : listNames){
+            i++;
+            if (i % 2 != 0){
+                stringName.append(i+". "+name);
+                if (i < listNames.size())
+                    stringName.append(", ");
+            }
+        }
+
+        return stringName.toString();
+    }
+
+    public static String notAnEvenIndex2(List<String> listNames){
+        StringBuilder arg0 = new StringBuilder();
+        arg0.append("/0");
+        String stringName = listNames
+                .stream()
+                .reduce(arg0,
+                        (arg, s) -> {
+                            String[] arrayStr = arg.toString().split("/");
+                            arg.delete(0,arg.length());
+                            arg.append(arrayStr[0]);
+                            int i = Integer.parseInt(arrayStr[1]);
+                            i++;
+                            arg.append((i % 2 != 0)? i+". "+s : "");
+                            arg.append((i % 2 != 0) && (i < listNames.size())? ", " : "");
+                            arg.append((i < listNames.size())? "/"+i : "");
+                            return arg;
+                        },(a1, a2) -> null
+                        ).toString();
+
+        return stringName;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Task1.notAnEvenIndex(
+                Stream.of("John", "Bill", "Ivan", "Peter", "Kolya")
+                .collect(Collectors.toList())
+        ));
+
+        System.out.println("**********II*************");
+
+        List<String> listName = new LinkedList<>();
+        listName.add("John");
+        listName.add("Bill");
+        listName.add("Ivan");
+        listName.add("Peter");
+        listName.add("Kolya");
+        System.out.println(Task1.notAnEvenIndex2(listName ));
+    }
+}
